a49845b7a72ce9eb4336ce4025299a24a66c9a4f
Thomas PEREZ <92369255+ThomasPerez91@users.noreply.github.com>
2023-08-17 09:38:47 +0200

    feat: add Docker + configs (#2)
    Co-authored-by: Diall Thydus <pro.thomas.perez@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Archea-Vault-API
+## Installation
+
+**Clone the repository:**
+```
+git clone https://github.com/Pixel-Conqueror/Archea-Vault-API.git
+```
+
+**Enter directory:**
+```
+cd Archea-Vault-API
+```
+
+**Install dependencies:**
+```
+npm install
+```
+
+**Create .env:**
+```
+cp .env.exemple .env
+```
+
+**Create container:**
+```
+docker-compose up -d
+```
+
+
--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,21 @@
 |
 */
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
   return { hello: 'world' }
+})
+
+//Authentication
+Route.post('/register', 'AuthController.register')
+Route.post('/login', 'AuthController.login')
+Route.get('/logout', 'AuthController.logout').middleware(['token', 'auth'])
+
+//Monitoring
+Route.get('/health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy ? response.ok(report) : response.badRequest(report)
 })
